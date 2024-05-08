08d895cc1b8c68a3b4e9ce2c084d93e3c8797439
Dmitrii Ostapchuk <dmit.ostapchuk@gmail.com>
2024-05-08 11:23:20 +0500

    try to correct redirection

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -8,7 +8,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/add")
@@ -23,8 +22,8 @@ public class AddUserServlet extends HttpServlet {
             User user = new User(firstName, lastName);
             warehouse.addUser(user);
             req.setAttribute("user", user);
-            req.getRequestDispatcher("/add.jsp").forward(req, resp);
-        } catch (ServletException e) {
+            resp.sendRedirect("/add.jsp");
+        } catch (Exception e) {
             e.printStackTrace();
             resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Failed to add user");
         }
--- src/main/java/com/example/servlet/GetUsersServlet.java
+++ src/main/java/com/example/servlet/GetUsersServlet.java
@@ -8,7 +8,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/users")
@@ -18,8 +17,8 @@ public class GetUsersServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
             req.setAttribute("users", warehouse.getUsers());
-            req.getRequestDispatcher("/users.jsp").forward(req, resp);
-        } catch (ServletException e) {
+            resp.sendRedirect("/users.jsp");
+        } catch (Exception e) {
             e.printStackTrace();
             resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Failed to retrieve users");
         }
